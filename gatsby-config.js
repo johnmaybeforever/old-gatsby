710bee8e08be6074ef8e8d4c4653cbcc3ee57847
jm <johnmaybeforever@outlook.com>
2021-03-21 23:08:52 -0400

    init commit

---
+++ gatsby-config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  siteMetadata: {
+    title: "john-site",
+  },
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "markdown-pages",
+        path: "./src/pages/markdown",
+      }
+    },
+    "gatsby-transformer-remark",
+  ],
+};
---
+++ src/pages/markdown/referrals.md
@@ -0,0 +1,5 @@
+---
+slug: "/referrals"
+date: "2021-03-21"
+title: "Referrals"
+---
